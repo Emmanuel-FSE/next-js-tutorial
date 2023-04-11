90cfbc2447a61166ecb968c8f734f2a209d92cd3
emmanuelmutisya254@gmail.com <EmmanuelTheWebDeveloper>
2023-04-11 16:01:47 +0300

    Single blog page implementation

--- src/pages/blogs/[blogId]/index.jsx
+++ src/pages/blogs/[blogId]/index.jsx
@@ -1,26 +1,47 @@
-import React from 'react'
-import { useRouter } from 'next/router'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
+import Nav from "@/components/Nav";
 
-function BlogDetails() {
-  const { query } = useRouter()
+function BlogDetails({ post }) {
   return (
-    <div className='container mx-auto'>
-      <h1>Blog details {query.blogId}</h1>
-      <Link className='bg-green-500 px-2 py-1 rounded-lg ' href='/blogs'>Go back to blog listing</Link>
+    <div>
+      <Nav />
+      <div className="container mx-auto">
+        <h1 className="font-bold text-2xl underline mt-4">{post.title}</h1>
+        <p className="mt-6 border shadow-md p-4">{post.body}</p>
+        <Link
+          className="bg-black text-slate-200 font-serif shadow-md text-bold px-2 py-1 rounded-sm fixed bottom-0 right-0 m-4"
+          href="/blogs"
+        >
+          {`<< Go back to blog listing`}
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default BlogDetails
+export default BlogDetails;
 
-// export async function getStaticProps() {
-//   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-//   const data = await res.json();
+export async function getStaticPaths() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+  const paths = data.map((post) => ({
+    params: { blogId: `${post.id}` },
+  }));
 
-//   return {
-//     props: {
-//       post: data,
-//     },
-//   };
-// }
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.blogId}`
+  );
+  const data = await res.json();
+
+  return {
+    props: {
+      post: data,
+    },
+  };
+}
